ddfb4daeeae0567c3cd6f44c807938947c9215f6
TheRealM18 <robertmacr@gmail.com>
2019-08-13 11:42:25 -0300

    Bumped Version

--- src/main/java/com/therealm18/mineandslash/expansion/Ref.java
+++ src/main/java/com/therealm18/mineandslash/expansion/Ref.java
@@ -3,7 +3,7 @@ package com.therealm18.mineandslash.expansion;
 public class Ref {
 
     public static final String MODID = "mineandslashexpansion";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     public static final String MOD_NAME = "Mine and Slash - Extension";
     public static final String NAME_NO_SPACE = "Mine_and_Slash_Extension";
     public static final String NAME_NO_SPACE_OR_SLASH = "MineAndSlashExtension";
