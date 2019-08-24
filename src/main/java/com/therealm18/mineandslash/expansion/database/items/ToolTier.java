0bf3367ccfff00d005975a4d1847e6ff222c6f1e
TheRealM18 <robertmacr@gmail.com>
2019-08-24 14:21:56 -0300

    Fixed issue with repairing

--- src/main/java/com/therealm18/mineandslash/expansion/database/items/ToolTier.java
+++ src/main/java/com/therealm18/mineandslash/expansion/database/items/ToolTier.java
@@ -1,29 +1,47 @@
 package com.therealm18.mineandslash.expansion.database.items;
 
+import java.util.function.Supplier;
+
+import net.minecraft.block.Blocks;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyLoadBase;
 
 public enum ToolTier implements IItemTier {
 	
-   COMMON(0, 128, 4.0F, 1.0F, 30),
-   UNCOMMON(1, 256, 8.0F, 2.0F, 30),
-   RARE(2, 512, 12.0F, 4.0F, 30),
-   EPIC(3, 1024, 16.0F, 8.0F, 30),
-   LEGENDARY(4, 3048, 20.0F, 16.0F, 30),
-   MYTHICAL(5, 4096, 24.0F, 32.0F, 30);
+   COMMON(0, 128, 4.0F, 1.0F, 30, () -> {
+	      return Ingredient.fromItems(Blocks.AIR);
+	   }),
+   UNCOMMON(1, 256, 8.0F, 2.0F, 30, () -> {
+	      return Ingredient.fromItems(Blocks.AIR);
+	   }),
+   RARE(2, 512, 12.0F, 4.0F, 30, () -> {
+	      return Ingredient.fromItems(Blocks.AIR);
+	   }),
+   EPIC(3, 1024, 16.0F, 8.0F, 30, () -> {
+	      return Ingredient.fromItems(Blocks.AIR);
+	   }),
+   LEGENDARY(4, 3048, 20.0F, 16.0F, 30, () -> {
+	      return Ingredient.fromItems(Blocks.AIR);
+	   }),
+   MYTHICAL(5, 4096, 24.0F, 32.0F, 30, () -> {
+	      return Ingredient.fromItems(Blocks.AIR);
+	   });
 
    private final int harvestLevel;
    private final int maxUses;
    private final float efficiency;
    private final float attackDamage;
    private final int enchantability;
+   private final LazyLoadBase<Ingredient> repairMaterial;
 
-   private ToolTier(int harvestLevelIn, int maxUsesIn, float efficiencyIn, float attackDamageIn, int enchantabilityIn) {
+   private ToolTier(int harvestLevelIn, int maxUsesIn, float efficiencyIn, float attackDamageIn, int enchantabilityIn, Supplier<Ingredient> repairMaterialIn) {
       this.harvestLevel = harvestLevelIn;
       this.maxUses = maxUsesIn;
       this.efficiency = efficiencyIn;
       this.attackDamage = attackDamageIn;
       this.enchantability = enchantabilityIn;
+      this.repairMaterial = new LazyLoadBase<>(repairMaterialIn);
    }
 
    public int getMaxUses() {
@@ -46,8 +64,7 @@ public enum ToolTier implements IItemTier {
       return this.enchantability;
    }
 
-@Override
-public Ingredient getRepairMaterial() {
-	return null;
-}
+   public Ingredient getRepairMaterial() {
+      return this.repairMaterial.getValue();
+   }
 }
