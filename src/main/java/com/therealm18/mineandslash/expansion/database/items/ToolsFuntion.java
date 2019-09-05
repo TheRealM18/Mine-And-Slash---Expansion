77add7af7956c800f1d2c1770ca93243659e4078
TheRealM18 <robertmacr@gmail.com>
2019-09-05 11:18:51 -0300

    More work on fixing hammers

--- src/main/java/com/therealm18/mineandslash/expansion/database/items/ToolsFuntion.java
+++ src/main/java/com/therealm18/mineandslash/expansion/database/items/ToolsFuntion.java
@@ -85,12 +85,12 @@ public class ToolsFuntion {
         Vec3d vec3d = player.getEyePosition(1.0F);
         float f2 = MathHelper.cos(-f1 * ((float)Math.PI / 180F) - (float)Math.PI);
         float f3 = MathHelper.sin(-f1 * ((float)Math.PI / 180F) - (float)Math.PI);
-        float f4 = -MathHelper.cos(-f * ((float)Math.PI / 180F));
+        float f4 = MathHelper.cos(-f * ((float)Math.PI / 180F));
         float f5 = MathHelper.sin(-f * ((float)Math.PI / 180F));
         float f6 = f3 * f4;
         float f7 = f2 * f4;
         double d0 = player.getAttribute(PlayerEntity.REACH_DISTANCE).getValue();;
-        Vec3d vec3d1 = vec3d.add((double)f6 * d0, (double)f5 * d0, (double)f7 * d0 + 1);
+        Vec3d vec3d1 = vec3d.add((double)f6 * d0, (double)f5 * d0, (double)f7 * d0);
         return worldIn.rayTraceBlocks(new RayTraceContext(vec3d, vec3d1, RayTraceContext.BlockMode.OUTLINE, fluidMode, player));
     }
 }
